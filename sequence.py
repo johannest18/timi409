a4688cbda5658cee069a23061a3e3ebae10fd5a0
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-09-04 14:52:33 +0000

    Algorithm written for seqcuence

--- sequence.py
+++ sequence.py
@@ -1,2 +1,3 @@
-#TAka við jákvæðri heiltölu í input
-#
+#TAka við jákvæðri heiltölu í input, köllum hana n
+#Gera formúlu sem að finnur allar í runu þar sem að runa byrjar á 1 og tölurnar hækka um summu síðustu þriggja talna í runu í hvert skipti.
+#Prenta út n- fyrstu tölurnar í formúlunni
