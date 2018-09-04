f9f966837f852acd304e2beea7965bb6bbf2df08
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-09-04 14:32:09 +0000

    Program for max_int

--- max_int.py
+++ max_int.py
@@ -3,9 +3,17 @@ num_int = int(input("Input a number: "))    # Do not change this line
 #Láta forritið muna eftir tölunni
 max_int = num_int
 #Ef að ekki er skráð inn jákvæð heiltala (0 meðtalinn) þá er hún hæsta talan og skal því prenta hana út.
+if num_int<0:
+    print("The maximum is", max_int)
 #Aftur skal biðja um heiltölu í input.
 # Ef hún er hærri en hæsta talan sem forritið man eftir skal forritið gleyma þeirri tölu og muna þessa í staðinn.
 #Ef talan er neikvæð heiltala skal forritið prenta út hæstu tölu sem slegin hefur verið inn.
-# EF talan er jákvæð skal endurtaka frá skrefi 4
+# EF talan er jákvæð skal endurtaka
+
+while num_int >= 0:
+    num_int = int(input("Input a number: "))
+    if num_int > max_int:
+        max_int=num_int
+    
 
 print("The maximum is", max_int)    # Do not change this line
