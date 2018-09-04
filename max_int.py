bf41717ad90dee1d61ea1a958a8425f068cfc455
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-09-04 14:23:01 +0000

    Program for max_int

--- max_int.py
+++ max_int.py
@@ -1,8 +1,11 @@
 #taka við heiltölum í input
+num_int = int(input("Input a number: "))    # Do not change this line
 #Láta forritið muna eftir tölunni
+max_int = num_int
 #Ef að ekki er skráð inn jákvæð heiltala (0 meðtalinn) þá er hún hæsta talan og skal því prenta hana út.
 #Aftur skal biðja um heiltölu í input.
 # Ef hún er hærri en hæsta talan sem forritið man eftir skal forritið gleyma þeirri tölu og muna þessa í staðinn.
 #Ef talan er neikvæð heiltala skal forritið prenta út hæstu tölu sem slegin hefur verið inn.
 # EF talan er jákvæð skal endurtaka frá skrefi 4
-dksljflskdhfsldfj
+
+print("The maximum is", max_int)    # Do not change this line
