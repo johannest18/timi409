af7bd073d0ee452e0e4c5361a03f2938390bfe62
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-09-04 14:12:30 +0000

    Algorithm for max_int

--- max_int.py
+++ max_int.py
@@ -1,1 +1,7 @@
-#taka við tölum
+#taka við heiltölum í input
+#Láta forritið muna eftir tölunni
+#Ef að ekki er skráð inn jákvæð heiltala (0 meðtalinn) þá er hún hæsta talan og skal því prenta hana út.
+#Aftur skal biðja um heiltölu í input.
+# Ef hún er hærri en hæsta talan sem forritið man eftir skal forritið gleyma þeirri tölu og muna þessa í staðinn.
+#Ef talan er neikvæð heiltala skal forritið prenta út hæstu tölu sem slegin hefur verið inn.
+# EF talan er jákvæð skal endurtaka frá skrefi 4
