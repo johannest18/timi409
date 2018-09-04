b5ccc2bcdb60bb44fdb61d3f4a4a40fb63196de3
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-09-04 14:55:31 +0000

    Algorithm written for seqcuence

--- sequence.py
+++ sequence.py
@@ -1,3 +1,4 @@
 #TAka við jákvæðri heiltölu í input, köllum hana n
+# Ef talan er 1 skal prenta út lista bara með töluni 1. Ef talan er 2 skal prenta út lista með 1,2 og ef hún er 3 skal prenta út lista með 1, 2 og 3.
 #Gera formúlu sem að finnur allar í runu þar sem að runa byrjar á 1 og tölurnar hækka um summu síðustu þriggja talna í runu í hvert skipti.
 #Prenta út n- fyrstu tölurnar í formúlunni
